0199300add8e798ca5f8b94d934a1c6cc8876920
Vladislav <kak60g@gmail.com>
2018-08-02 12:50:14 +0300

    add myself map and characters

--- js/game.js
+++ js/game.js
@@ -5,14 +5,19 @@
 
 var Game = {};
 
+const dinos = ['doux', 'mort', 'tard', 'vita']
+
 Game.init = function(){
     game.stage.disableVisibilityChange = true;
 };
 
 Game.preload = function() {
-    game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
-    game.load.image('sprite','assets/sprites/sprite.png');
+    game.load.tilemap('map', 'assets/map/map1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('tileset', 'assets/map/tilesheet1.png',32,32);
+
+    dinos.forEach((name) => {
+        game.load.spritesheet(name, `assets/sprites/DinoSprites - ${name}.png`,24,24);
+    })
 };
 
 Game.create = function(){
@@ -20,7 +25,7 @@ Game.create = function(){
     var testKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     testKey.onDown.add(Client.sendTest, this);
     var map = game.add.tilemap('map');
-    map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
+    map.addTilesetImage('tilesheet1', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
     var layer;
     for(var i = 0; i < map.layers.length; i++) {
         layer = map.createLayer(i);
@@ -35,13 +40,18 @@ Game.getCoordinates = function(layer,pointer){
 };
 
 Game.addNewPlayer = function(id,x,y){
-    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+    Game.playerMap[id] = game.add.sprite(x,y,dinos[Math.floor(Math.random() * dinos.length)]);
+    Game.playerMap[id].animations.add('move', [...Array(10).keys()], 10, true);
 };
 
 Game.movePlayer = function(id,x,y){
     var player = Game.playerMap[id];
+    player.animations.play('move');
     var distance = Phaser.Math.distance(player.x,player.y,x,y);
     var tween = game.add.tween(player);
+
+    tween.onComplete.add(() => player.animations.stop(), this);
+
     var duration = distance*10;
     tween.to({x:x,y:y}, duration);
     tween.start();
@@ -51,3 +61,25 @@ Game.removePlayer = function(id){
     Game.playerMap[id].destroy();
     delete Game.playerMap[id];
 };
+
+checkOverlap = (spriteA, spriteB) => {
+
+    var boundsA = spriteA.getBounds();
+    var boundsB = spriteB.getBounds();
+
+    return Phaser.Rectangle.intersects(boundsA, boundsB);
+
+}
+
+Game.update = () => {
+    const players = Object.values(Game.playerMap)
+    if (!!players.length) {
+        console.log(Game.playerMap)
+
+        if (checkOverlap(players[0], players[1]))
+        {
+            console.log('Overlapping: true');
+        }
+    }
+
+}
